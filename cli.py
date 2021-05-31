9d2489e2be508a3f433a0c7902ce95152a009857
Rendy Arya Kemal <rendyarya22@gmail.com>
2021-05-31 10:42:37 +0700

    Fix cli tooling and give out types for parser.

--- cli.py
+++ cli.py
@@ -4,31 +4,35 @@ import pixel_extract
 
 parser = argparse.ArgumentParser(description="Generate storyboard for Bad Apple.")
 parser.add_argument("outfile", help="Output .osb filename.")
-parser.add_argument("size", help="Size of each square in storyboard.")
+parser.add_argument("size", help="Size of each square in storyboard.", type=int)
 parser.add_argument(
     "-j",
     "--jobs",
     default=2,
     help="Number of threads to spawn.",
     metavar="N",
+    type=int,
 )
 parser.add_argument(
     "--splits",
     default=16,
     help="Number of splits to generate.",
     metavar="N",
+    type=int,
 )
 parser.add_argument(
     "--fps",
     default=30,
     help="FPS of the target storyboard.",
     metavar="N",
+    type=int,
 )
 parser.add_argument(
     "--transparency",
     default=1,
     help="Transparency precision level.",
     metavar="N",
+    type=int,
 )
 parser.add_argument(
     "--only-generate",
@@ -42,15 +46,15 @@ if __name__ == "__main__":
     if not args.only_generate:
         print("> Extracting pixel data")
         pixel_extract.run(
-            args.obj_size,
+            args.size,
             number_of_thread=args.jobs,
             number_of_splits=args.splits,
         )
 
     print("> Generating osb")
     osb.generate_osb(
-        args.obj_size,
-        args.output_filename,
+        args.size,
+        args.outfile,
         fps=args.fps,
         transparency_precision=args.transparency,
     )
